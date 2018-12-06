46aed59a8d79e26a2e324b02c2f40af382ca38bf
Nick Steger <nsteger@iastate.edu>
2018-12-06 15:31:58 -0600

    Add TwilioUtil as spring component, can now send sms text

--- src/main/java/com/nicksteger/dailyreport/interfaces/twilio/TwilioUtil.java
+++ src/main/java/com/nicksteger/dailyreport/interfaces/twilio/TwilioUtil.java
@@ -1,18 +1,30 @@
 package com.nicksteger.dailyreport.interfaces.twilio;
 
+import com.nicksteger.dailyreport.data.entity.User;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import com.twilio.Twilio;
 
 @Component
 public class TwilioUtil {
 
     private String sid;
     private String token;
+    private String fromNumber;
 
     public TwilioUtil(@Value("${twilio.auth.sid}") String sid,
-                      @Value("${twilio.auth.token}") String token) {
+                      @Value("${twilio.auth.token}") String token,
+                      @Value("${twilio.account.number}") String fromNumber) {
         this.sid = sid;
         this.token = token;
+        this.fromNumber = fromNumber;
+        Twilio.init(sid, token);
+    }
+
+    public Message sendMessage(User user, String message) {
+       return Message.creator(new PhoneNumber(user.getSmsNumber()), new PhoneNumber(fromNumber), message).create();
     }
 
 }
--- src/test/java/com/nicksteger/dailyreport/interfaces/twilio/TwilioUtil_Test.java
+++ src/test/java/com/nicksteger/dailyreport/interfaces/twilio/TwilioUtil_Test.java
@@ -1,15 +1,29 @@
 package com.nicksteger.dailyreport.interfaces.twilio;
 
+import com.nicksteger.dailyreport.data.entity.User;
+import com.twilio.rest.api.v2010.account.Message;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TwilioUtil_Test {
 
+    @Autowired
     private TwilioUtil twilioUtil;
 
-    @Autowired
-    public TwilioUtil_Test(TwilioUtil twilioUtil) {
-        this.twilioUtil = twilioUtil;
+    @Test
+    public void testNotifyUser_validNum() {
+        User testUser = new User();
+        testUser.setSmsNumber("+15634519000");
+        String testMessage = "Sent from your Twilio trial account - Hello from DailyReport!";
+        Message message = twilioUtil.sendMessage(testUser, testMessage);
+        assertEquals(testMessage, message.getBody());
     }
+
 }
